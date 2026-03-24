bfc24c1aeab6615f3a5cd224b2ab97236ceee2d2
kimhong <try.kimhong@institute.pse.ngo>
2026-03-24 14:51:08 +0700

    update connection

--- backend/src/config/db.ts
+++ backend/src/config/db.ts
@@ -1,17 +1,22 @@
-import { Pool } from "pg";
+import "reflect-metadata";
+import { DataSource } from "typeorm";
 import dotenv from "dotenv";
 
 dotenv.config();
 
-export const pool = new Pool({
-  user: process.env.DB_USER, // kimhong
-  host: process.env.DB_HOST, // localhost
-  database: process.env.DB_NAME, // volunteer_attendance
-  password: process.env.DB_PASSWORD, // try@kimhong123!
-  port: Number(process.env.DB_PORT) || 5432,
-});
+export const AppDataSource = new DataSource({
+  type: "postgres",
 
-pool.query("SELECT NOW()", (err, res) => {
-  if (err) console.error("DB Connection Error:", err);
-  else console.log("DB Connected. Time:", res.rows[0]);
+  url: process.env.DB_CONNECTION_STRING,
+
+  ssl: {
+    rejectUnauthorized: false, // 🔥 REQUIRED for Neon
+  },
+
+  synchronize: true, // ⚠️ only for development
+  logging: true,
+
+  entities: [__dirname + "/../entities/*.ts"],
+  migrations: [],
+  subscribers: [],
 });
